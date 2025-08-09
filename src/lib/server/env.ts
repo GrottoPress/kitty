GrottoPress/kitty

commit a6b3e9f7b5d4a8b9df2a3c6214444b3b96f8a6ba

Fix CI errors
See <https://github.com/GrottoPress/kitty/actions/runs/16842544164>.

src/lib/server/env.ts
@@ -1,12 +1,12 @@
 import { env } from '$env/dynamic/private'
 
 export const allowedRequestMethods =
-  env.ALLOWED_REQUEST_METHODS?.split(',')
+  env.ALLOWED_REQUEST_METHODS.split(',')
     .map(method => method.trim())
     .filter(method => method)
 
 export const csrfSkipRoutes =
-  env.CSRF_SKIP_ROUTES?.split(',')
+  env.CSRF_SKIP_ROUTES.split(',')
     .map(route => route.trim())
     .filter(route => route)
 

.github/workflows/test.yml
@@ -16,7 +16,7 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        nodejs: [20.x, latest]
+        nodejs: [20.x]
     runs-on: ubuntu-latest
     continue-on-error: false
     steps:
@@ -31,8 +31,18 @@ jobs:
         run: npm ci
       - name: Lint code
         run: npm run lint
+        env:
+          ALLOWED_REQUEST_METHODS: GET,HEAD,POST
+          CSRF_SKIP_ROUTES: /handlers/verify-csrf-token/ignore,/handlers/verify-csrf-token.json/ignore
+          SECRET_KEY: J9oyuTDuGSQhwE3lOutjUgXe4yfpWQtI # 32 bytes
+          SESSION_KEY: _kitty-session
       - name: Check code
         run: npm run check
+        env:
+          ALLOWED_REQUEST_METHODS: GET,HEAD,POST
+          CSRF_SKIP_ROUTES: /handlers/verify-csrf-token/ignore,/handlers/verify-csrf-token.json/ignore
+          SECRET_KEY: J9oyuTDuGSQhwE3lOutjUgXe4yfpWQtI # 32 bytes
+          SESSION_KEY: _kitty-session
   tests__unit:
     strategy:
       fail-fast: false
@@ -74,3 +84,8 @@ jobs:
         run: npx playwright install --with-deps --only-shell
       - name: Run tests
         run: npm run test:e2e
+        env:
+          ALLOWED_REQUEST_METHODS: GET,HEAD,POST
+          CSRF_SKIP_ROUTES: /handlers/verify-csrf-token/ignore,/handlers/verify-csrf-token.json/ignore
+          SECRET_KEY: J9oyuTDuGSQhwE3lOutjUgXe4yfpWQtI # 32 bytes
+          SESSION_KEY: _kitty-session

commit b7d6b7e5b493354cf8c0804f1e38043c9cd7469e

Fix build error
```
TypeError [Error]: Cannot read properties of undefined (reading 'split')
    at file:///tmp/kit/.svelte-kit/output/server/chunks/env.js:66:67
    at ModuleJob.run (node:internal/modules/esm/module_job:329:25)
    at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:644:26)
    at async analyse (file:///tmp/kit/node_modules/@sveltejs/kit/src/core/postbuild/analyse.js:120:62)
    at async MessagePort.<anonymous> (file:///tmp/kit/node_modules/@sveltejs/kit/src/utils/fork.js:23:16)
```

This was a regression in commit a6b3e9f7b5d4a8b9df2a3c6214444b3b96f8a6ba.

src/lib/server/env.ts
@@ -1,12 +1,12 @@
 import { env } from '$env/dynamic/private'
 
 export const allowedRequestMethods =
-  env.ALLOWED_REQUEST_METHODS.split(',')
+  env.ALLOWED_REQUEST_METHODS?.split(',')
     .map(method => method.trim())
     .filter(method => method)
 
 export const csrfSkipRoutes =
-  env.CSRF_SKIP_ROUTES.split(',')
+  env.CSRF_SKIP_ROUTES?.split(',')
     .map(route => route.trim())
     .filter(route => route)
 