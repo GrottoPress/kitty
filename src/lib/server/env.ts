a6b3e9f7b5d4a8b9df2a3c6214444b3b96f8a6ba
akadusei <attakusiadusei@gmail.com>
2025-08-09 00:01:53 +0000

    Fix CI errors
    See <https://github.com/GrottoPress/kitty/actions/runs/16842544164>.

--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,12 +1,12 @@
 import { env } from '$env/dynamic/private'
 
 export const allowedRequestMethods =
-  env.ALLOWED_REQUEST_METHODS?.split(',')
+  env.ALLOWED_REQUEST_METHODS.split(',')
     .map(method => method.trim())
     .filter(method => method)
 
 export const csrfSkipRoutes =
-  env.CSRF_SKIP_ROUTES?.split(',')
+  env.CSRF_SKIP_ROUTES.split(',')
     .map(route => route.trim())
     .filter(route => route)
 
